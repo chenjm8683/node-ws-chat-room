a50d66cd359cb231f2d80ed14f85eed35c34333c
Jingming Chen <chenjm8683@gmail.com>
2016-02-02 22:23:59 -0800

    clientName function and enterMessage

--- client.js
+++ client.js
@@ -9,14 +9,55 @@ var rl = readline.createInterface({
 
 var ws;
 
+var sendMessage = function(type, content) {
+  ws.send( JSON.stringify(
+    {type: type, content: content}
+  ));
+};
+
+var displayMessage = function(message) {
+  // var type = data.type;
+  // var name = data.name;
+  // var content = data.content;
+  // switch (type) {
+  //   case 'connected':
+  //     console.log("     %s joined the chat room.", name);
+  //     break;
+  //   case 'message':
+  //     console.log("%s says: %s", name, content);
+  //     break;
+  // }
+
+  console.log(message);
+};
+
+var enterMessage = function () {
+  rl.question('\n', function(content) {
+    sendMessage('message', content);
+    // rl.close();
+    enterMessage();
+  });
+};
+
 rl.question('Your name: ', function(name) {
   ws = new WebSocket(SERVER_URL);
-  ws.on('open', function open() {
-    ws.send( JSON.stringify(
-      {type: "name", data: name}
-    ));
+  ws.on('open', function () {
+    sendMessage('init', name);
   });
+  ws.on('message', function (message) {
+    // displayMessage(JSON.parse(data));
+    displayMessage(message);
+  });
+  // rl.close();
+  enterMessage()
 });
+
+
+
+
+
+
+
 
 
 
--- server.js
+++ server.js
@@ -9,19 +9,34 @@ PORT = 8081;
 var WebSocketServer = require('ws').Server;
 var wss = new WebSocketServer({ port: 8081 });
 
-wss.broadcast = function(message) {
+wss.broadcast = function(content) {
   wss.clients.forEach(function (client) {
-    client.send(data);
+    client.send(content);
   });
 };
 
-wss.on('connection', function connection(ws) {
-  console.log("Client connected!")
-  ws.send('Connected to server');
+wss.on('connection', function (ws) {
+  // console.log("Client connected!")
+  // ws.send('Connected to server');
 
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
+  ws.on('message', function (data) {
+    data = JSON.parse(data);
+    console.log(data);
+    var type = data.type;
+    var content = data.content;
+    var message;
+    switch (type) {
+      case 'init':
+        ws.clientName = content;
+        message = "           " + ws.clientName + " joined the chat room.";
+        break;
+      case 'message':
+        message = ws.clientName + " says: " + content;
+        break;
+    }
+    console.log(message);
+    wss.broadcast(message);
   });
 
-  ws.send('Message received');
+  // ws.send('Message received');
 });
